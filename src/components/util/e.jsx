01d10b6725360dcda9382f255d401638035741e2
MahmoudFredj <mahmoudrayfredj@gmail.com>
2021-03-24 02:59:45 +0100

    utlil

---
+++ src/components/util/e.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+// <defs><style>.a{fill:#653d36;}.t{fill:#7daf3f;}</style></defs>
+const E = ({ className }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="42.09"
+            height="62.734"
+            viewBox="0 0 42.09 62.734"
+            class={className}
+        ><g
+            transform="translate(-122.347 -60.728)"
+        ><path
+                    d="M223.7,23.548V12.186H181.611V23.548Z"
+                    transform="translate(-59.264 48.542)"
+                /><path
+                    d="M223.7,59.515H193.773V44.631H223.7V33.909h-42.09V70.877H223.7Z"
+                    transform="translate(-59.264 52.585)"
+                />
+            </g>
+        </svg>
+    );
+}
+
+export default E;
