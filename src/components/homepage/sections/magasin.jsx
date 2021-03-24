eabc77ad517ec413ee73c5193a4b3ef05117f8ee
MahmoudFredj <mahmoudrayfredj@gmail.com>
2021-03-24 22:42:28 +0100

    magasin

--- src/components/homepage/sections/magasin.jsx
+++ src/components/homepage/sections/magasin.jsx
@@ -1,6 +1,33 @@
 import React, { Component } from 'react';
+import background from "../../../assets/c49410fcd941eb66349f9dd27fceebaf.png";
+import saw from "../../../assets/image.png";
+import E from '../../util/e';
 const Magasin = () => {
-    return (<section></section>);
+    return (
+        <section className="magasin-panel">
+            <div className="background">
+
+                <E className="e" />
+                <img src={background} alt="too cool for school" />
+            </div>
+            <div className="content">
+                <h1>Notre magasin</h1>
+                <div className="off-content-left">
+
+                    <div className="content">
+                        <img src={saw} alt="saw" />
+                    </div>
+                    <div className="foot">
+                        Lorem ipsum dolor sit amet,
+                        consectetur adipisicing elit. Minima laborum nulla dicta totam quas
+                        voluptatem aliquam sequi eos sunt omnis cum quibusdam repellendus
+                        tenetur atque ipsam, natus doloribus, tempore incidunt.
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    );
 }
 
 export default Magasin;
