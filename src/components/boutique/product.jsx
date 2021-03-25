bb8cf4f4923b801bac140d62b3e8a62d25c5f30b
MahmoudFredj <mahmoudrayfredj@gmail.com>
2021-03-25 08:17:37 +0100

    finish boutique

--- src/components/boutique/product.jsx
+++ src/components/boutique/product.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import arrow from "../../assets/arrow-left-line.svg";
 class Product extends Component {
     state = {}
     render() {
@@ -7,6 +9,8 @@ class Product extends Component {
                 <img src={this.props.value.picture} alt="product-img" />
                 <p>{this.props.value.product_name}</p>
                 <label>{this.props.value.price}</label>
+                <Link to="#">Voire Detail <img className="arw-link-prdct" src={arrow} alt="arrow" /></Link>
+
             </div>
         );
     }
